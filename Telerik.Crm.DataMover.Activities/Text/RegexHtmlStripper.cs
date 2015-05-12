815d6d767f503d8d99aee5ac79faf91a53b75990
KristianKirov <kristiankirov9112@gmail.com>
2015-05-12 17:59:39 +0300

    Enhanced html stripping

--- Telerik.Crm.DataMover.Activities/Text/RegexHtmlStripper.cs
+++ Telerik.Crm.DataMover.Activities/Text/RegexHtmlStripper.cs
@@ -10,16 +10,28 @@ namespace Telerik.Crm.DataMover.Activities.Text
 {
 	public class RegexHtmlStripper : IHtmlStripper
 	{
-		private readonly Regex removeTagsRegex = new Regex("<[^>]+>|&nbsp;", RegexOptions.Compiled);
+		private readonly Regex removeCommentsRegex = new Regex("<!--.+?-->", RegexOptions.Singleline | RegexOptions.Compiled);
+
+		private readonly Regex removeStylesRegex = new Regex("<style.+?</style>", RegexOptions.Singleline | RegexOptions.Compiled);
+
+		private readonly Regex removeScriptsRegex = new Regex("<script.+?</script>", RegexOptions.Singleline | RegexOptions.Compiled);
+
+		private readonly Regex removeSinglelineScriptsRegex = new Regex("<script.+?/>", RegexOptions.Singleline | RegexOptions.Compiled);
+
+		private readonly Regex removeTagsRegex = new Regex("<.+?>|&nbsp;", RegexOptions.Singleline | RegexOptions.Compiled);
 
 		private readonly Regex removeWhitespacesRegex = new Regex("\\s{2,}|\\n|\\r", RegexOptions.Compiled);
 
 		public string Strip(string html)
 		{
-			string htmlWithoutTags = removeTagsRegex.Replace(html, string.Empty);
-			string htmlWithoutMultipleSpacesAndNewLines = removeWhitespacesRegex.Replace(htmlWithoutTags, " ");
+			string strippedHtml = this.removeCommentsRegex.Replace(html, string.Empty);
+			strippedHtml = this.removeStylesRegex.Replace(strippedHtml, string.Empty);
+			strippedHtml = this.removeScriptsRegex.Replace(strippedHtml, string.Empty);
+			strippedHtml = this.removeSinglelineScriptsRegex.Replace(strippedHtml, string.Empty);
+			strippedHtml = this.removeTagsRegex.Replace(strippedHtml, string.Empty);
+			strippedHtml = this.removeWhitespacesRegex.Replace(strippedHtml, " ");
 
-			return htmlWithoutMultipleSpacesAndNewLines;
+			return strippedHtml;
 		}
 	}
 }
